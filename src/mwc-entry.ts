e5aa45f515a7a72088e656993407ab222ceee048
Kevin Bimonte <kbimonte@gmail.com>
2025-11-05 14:54:07 -0500

    other: maybe switching to dropdowns
    Signed-off-by: Kevin Bimonte <kbimonte@gmail.com>

--- src/mwc-entry.ts
+++ src/mwc-entry.ts
@@ -1,11 +1,7 @@
 // 1. Import and register all Material Web Components
 import '@material/web/all.js';
 
-import {faWindows} from "@fortawesome/free-brands-svg-icons"
-
 import {MdMenu} from "@material/web/all";
-import {setupPrimaryTabPanels} from "./homepage";
-
 
 
 // 2. Custom MkDocs Initialization Logic
@@ -14,19 +10,25 @@ const document$ = window.document$;
 if (document$) {
     // Subscribe to run logic on initial load AND every instant navigation
     document$.subscribe(() => {
-        downloadMacOs();
-        setupPrimaryTabPanels();
+        // downloadMacOs();
+        // setupPrimaryTabPanels();
     });
+
+    // const cached = __md_get<SourceFacts>("__source", sessionStorage)
+    const gitInformation: string | null = getValueFromSessionStoragePartialMatch("__source");
+
+    console.log(gitInformation)
 }
 
-function downloadMacOs() {
-    // This example uses anchor as an ID reference
-    const anchorEl: HTMLElement | null = document.body.querySelector('#usage-anchor');
-    const menuEl: MdMenu | null = document.body.querySelector('#usage-menu');
-
-    if (anchorEl && menuEl) {
-        anchorEl.addEventListener('click', () => {
-            menuEl.open = !menuEl.open;
-        });
+function getValueFromSessionStoragePartialMatch(partialMatch: string): string | null {
+    for (let i = 0; i < sessionStorage.length; i++) {
+        const key = sessionStorage.key(i);
+        // Ensure the key exists and perform the partial match check
+        if (key && key.includes(partialMatch)) {
+            // Retrieve the value for the matching key
+            return sessionStorage.getItem(key);
+        }
     }
+    // Return null if no key matches the partial string
+    return null;
 }
